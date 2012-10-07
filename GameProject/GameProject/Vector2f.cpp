5a04e5e2b530d83306fc617e41b8e1aa7e398f0f
Christian Holt <ymabob@gmail.com>
2012-10-08 00:22:07 +0200

    Added vector class

---
+++ GameProject/GameProject/Vector2f.cpp
@@ -0,0 +1,142 @@
+#include "Vector2f.h"
+
+////////////////////// CONSTRUCTORS & DESTRUCTOR ///////////////////////
+
+// Default constructor that sets X and Y to 0.  PRIVATE??
+Vector2f::Vector2f()
+{
+	x = 0;
+	y = 0;
+}
+
+// Constructor that creates a vector with the given (X, Y). Could be coordinate (X, Y) 
+Vector2f::Vector2f(float newX, float newY)
+{
+	x = newX;
+	y = newY;
+}
+
+Vector2f::~Vector2f()
+{
+	// Nothing?
+}
+
+////////////////////// VECTOR OPERATORS ///////////////////////
+
+// Returns the sum of the two vectors.
+Vector2f Vector2f::operator+ (Vector2f vector)
+{
+	return Vector2f(x + vector.x, y + vector.y);
+}
+
+// Returns the difference between the two vectors.
+Vector2f Vector2f::operator- (Vector2f vector)
+{
+	return Vector2f(x - vector.x, y - vector.y);
+}
+
+// Returns the component-wise multiplication of the vectors
+Vector2f Vector2f::operator* (Vector2f vector)
+{
+	return Vector2f(x * vector.x, y * vector.y);
+}
+
+// Returns the component-wise division of the vectors
+Vector2f Vector2f::operator/ (Vector2f vector)
+{
+	return Vector2f(x / vector.x, y / vector.y);
+}
+
+// Sets the vector equal to the parameter vector. 
+void Vector2f::operator= (Vector2f vector)
+{
+	x = vector.x;
+	y = vector.y;
+}
+
+////////////////////// SCALAR OPERATORS ///////////////////////
+
+// Returns the vector with all components multiplied by the scalar parameter
+Vector2f Vector2f::operator* (float scalar)
+{
+	return Vector2f(x * scalar, y * scalar); 
+}
+
+// Returns the vector with all components divided by the scalar parameter
+Vector2f Vector2f::operator/ (float scalar)
+{
+	return Vector2f(x / scalar, y / scalar);
+}
+
+
+
+////////////////////// PRODUCTS ///////////////////////
+
+// returns the DOT product between the the two vectors
+float Vector2f::dot (Vector2f vector)
+{
+	return (x * vector.x + y * vector.y);
+}
+
+// Returns the CROSS product between the two vectors
+float Vector2f::cross (Vector2f vector)
+{
+	return (x * vector.y + y * vector.x);
+}
+
+
+
+////////////////////// LENGTHS ///////////////////////
+
+// returns the length of the vector
+float Vector2f::length()
+{
+	return sqrt(x * x + y * y);
+}
+
+// Returns the square of the length of the vector. Useful when you just want to compare two vectors
+// to see which is longest, as this avoids computing the square roots. 
+float Vector2f::squaredLength()
+{
+	return (x * x + y * y);
+}
+
+// Returns a vector pointing in the same direction, but with unit length (lenght of 1)
+Vector2f Vector2f::unit()
+{
+	float length = sqrt(x * x + y * y);
+	return Vector2f(x / length, y / length);
+}
+
+
+
+////////////////////// ROTATION AND ANGLE ///////////////////////
+
+// NOTE: turn functions assume y-axis points up, otherwise they switch meaning (left = right, right = left)
+// Returns the vector rotated 90 degrees to the left. Useful for computing normals.
+Vector2f Vector2f::turnLeft()
+{
+	return Vector2f(-y, x);
+}
+
+// Returns the vector rotated 90 degrees to the right. Useful for computing normals. 
+Vector2f Vector2f::turnRight()
+{
+	return Vector2f(y, -x);
+}
+
+// Rotates the vector by the specified angle. This is equvivalent to multiplying by the 2x2 rotation matrix.
+Vector2f Vector2f::rotate(float angle)
+{
+	float newX = x * cos(angle) - y * sin(angle);
+	float newY = x * sin(angle) + y * cos(angle);
+
+	return Vector2f(newX, newY);
+}
+
+// Returns the angle that the vector points to.
+// Note: atan2 Computes arc tangent with two parameters
+float Vector2f::angle()
+{
+	return atan2(y, x);
+}
---
+++ GameProject/GameProject/Vector2f.h
@@ -0,0 +1,55 @@
+//																	  //
+//	Purpose:	Implement a 2D vector in the MATHEMATICAL sense, not  //
+//				in the dynamic array sense (as STL vector is)		  //
+//	Author:		Christian Holt										  //
+//	Created:	06/10/2012											  //
+//  Revised:	**/**/****											  //
+//																	  //
+//																	  //
+////////////////////////////////////////////////////////////////////////
+
+
+#pragma once		// Makes sure it is only included once per compile.
+
+#include <math.h>
+
+class Vector2f
+{
+public:		// Hmm private??
+	float x;
+	float y;
+
+	 
+	// Constructors & Destructor
+	Vector2f();								// Default constructor that sets X and Y to 0. 
+	Vector2f(float x, float y);				// Constructor that creates a vector with the given (X, Y).
+	~Vector2f();							// Destructor. 
+
+	// Vector Operators
+	Vector2f operator+ (Vector2f vector);	// Returns the sum of the two vectors.
+	Vector2f operator- (Vector2f vector);   // Returns the difference between the two vectors.
+	Vector2f operator* (Vector2f vector);	// Returns the component-wise multiplication of the vectors
+	Vector2f operator/ (Vector2f vector);	// Returns the component-wise division of the vectors
+	void operator= (Vector2f vector);		// Sets the vector equal to the parameter vector. 
+
+	// Scalar operators
+	Vector2f operator* (float scalar);		// Returns the vector with all components multiplied by the scalar parameter
+	Vector2f operator/ (float scalar);		// Returns the vector with all components divided by the scalar parameter
+
+	// products
+	float dot (Vector2f vector);			// returns the DOT product between the the two vectors
+	float cross(Vector2f vector);			// Returns the CROSS product between the two vectors
+
+	// Lengths
+	float length();							// returns the length of the vector
+	Vector2f unit();						// Returns a vector pointing in the same direction, but with unit length (lenght of 1)
+	float squaredLength();					// Returns the square of the length of the vector. Useful when you just want to compare two vectors
+											// to see which is longest, as this avoids computing the square roots. 
+
+	// Rotation and angle
+											// NOTE: turn functions assume y-axis points up, otherwise they switch meaning (left = right, right = left)
+	Vector2f turnLeft();					// Returns the vector rotated 90 degrees to the left. Useful for computing normals.
+	Vector2f turnRight();					// Returns the vector rotated 90 degrees to the right. Useful for computing normals. 
+	Vector2f rotate(float angle);			// Rotates the vector by the specified angle.
+	float angle();							// Returns the angle that the vector points to.
+};
