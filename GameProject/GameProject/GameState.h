21a5fbdc73611e9f147ad655e2a96a3cb6aee305
Christian Holt <ymabob@gmail.com>
2012-10-13 12:50:31 +0200

    Modified game

--- GameProject/GameProject/GameState.h
+++ GameProject/GameProject/GameState.h
@@ -1,22 +1,22 @@
-#pragma once
-
-#include "game.h"
-
-// Abstract state class.. 
-class GameState
-{
-public:
-	virtual void init() = 0;
-	virtual void cleanup() = 0;
-	
-	virtual void pause() = 0;
-	virtual void resume() = 0;
-
-	virtual void handleEvents(Game* game) = 0;
-	virtual void update(Game* game) = 0;
-	virtual void draw(Game* game) = 0;
-
-	void changeState(Game* game, GameState* state ){
-		game -> changeState(state);
-	}
-};
+//#pragma once
+//
+//#include "game.h"
+//
+//// Abstract state class.. 
+//class GameState
+//{
+//public:
+//	virtual void init() = 0;
+//	virtual void cleanup() = 0;
+//	
+//	virtual void pause() = 0;
+//	virtual void resume() = 0;
+//
+//	virtual void handleEvents(Game* game) = 0;
+//	virtual void update(Game* game) = 0;
+//	virtual void draw(Game* game) = 0;
+//
+//	void changeState(Game* game, GameState* state ){
+//		game -> changeState(state);
+//	}
+//};
