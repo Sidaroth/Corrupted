9a23d837f9f8bd7c5a601f5506191dbf9efb7537
Øyvind Nordli <oe.nordli@gmail.com>
2012-10-23 23:40:33 +0200

    player collision and start coordinates added in EnvironmentHandler

--- GameProject/GameProject/Environment.h
+++ GameProject/GameProject/Environment.h
@@ -4,6 +4,7 @@
 */
 #include "SFML/Graphics.hpp"
 #include <vector>
+#include "Vector2f.h"
 
 class EnvironmentHandler
 {
@@ -16,12 +17,15 @@ private:
 	};
 	int TILESIZE;
 
+	Vector2f m_playerPosition;
 	sf::Image m_bitmap;
 	sf::Texture m_tiles;
+	sf::Image m_objectBitmap;
 	int m_iHorizontalBitmapSize;
 	int m_iVerticalBitmapSize;
 	std::vector<RGB*> m_floorArray;
 	std::vector<sf::Sprite> m_sprites;
+	std::vector<bool> m_objects;
 
 public:
 	EnvironmentHandler();
@@ -31,4 +35,7 @@ public:
 	void createLevel();
 	sf::IntRect colorChart(struct RGB);
 	void checkWalls();
+	void findObjects();
+	bool checkCollision( Vector2f playerPosition );
+	Vector2f getPlayerPosition( );
 };
