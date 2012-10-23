1c7a00256d43f7aea302622162c28407cdc2908a
Christian Holt <ymabob@gmail.com>
2012-10-23 21:46:47 +0200

    oopsie, forgot files in last commit

---
+++ GameProject/GameProject/Skeleton.cpp
@@ -0,0 +1,11 @@
+#include "Skeleton.h"
+
+Skeleton::Skeleton()
+{
+
+}
+
+void Skeleton::sayHello()
+{
+	std::cout << "Hello!\n";
+}
---
+++ GameProject/GameProject/Skeleton.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "Enemy.h"
+#include <iostream>
+
+class Skeleton : public Enemy
+{
+protected:
+
+public:
+	Skeleton();
+	void sayHello();
+};
