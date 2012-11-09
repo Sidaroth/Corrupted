1867d7afdd4f7c8026f37cac4069511f3d4f2a2e
Christian Stokkebekk <gfzilter@gmail.com>
2012-11-09 17:38:49 +0100

    some ui stuff

--- GameProject/GameProject/Ui.cpp
+++ GameProject/GameProject/Ui.cpp
@@ -14,12 +14,26 @@ bool Ui::loadContent()
 		return EXIT_FAILURE;
 	}
 
+	if(!m_healthDiamond.loadFromFile("../../Resources/healthDiamond.PNG"))
+	{
+		return EXIT_FAILURE;
+	}
+
+	if(!m_uiBackground.loadFromFile("../../Resources/GUI.PNG"))
+	{
+		return EXIT_FAILURE;
+	}
+
 	if(!font.loadFromFile("../../Resources/impact.ttf"))
 	{
 		std::cout << "Could not load font" << std::endl;
 	}
 
 	m_view.reset(sf::FloatRect(0, 0, 1366, 768));
+
+	m_healthDiamondSprite.setTexture(m_healthDiamond);
+	m_healthDiamondSprite.setPosition(75, 650);
+	m_healthDiamondSprite.setOrigin(50, 50);
 
 	m_uiBackgroundSprite.setTexture(m_uiBackground);
 	m_uiBackgroundSprite.setPosition(0, 0);
@@ -41,6 +55,8 @@ bool Ui::loadContent()
 	rightSideSecondaryStats.setPosition(1325, 583);
 	rightSideSecondaryStats.setScale(0.5, 0.5);
 	rightSideSecondaryStats.setFont(font);
+
+
 }
 
 void Ui::unloadContent()
@@ -63,6 +79,9 @@ void Ui::update(Player* player)
 	rightSideSecondaryStats.setString(numberToString(m_player->getCurrentSouls())+"\n\n"+
 										numberToString(m_player->getWeaponLevel())+"\n"+
 										numberToString(m_player->getArmorLevel()));
+
+	healthRotation = (101 - (m_player->getCurrentHealth()/m_player->getMaxHealth()*100));
+	m_healthDiamondSprite.setRotation(healthRotation);
 }
 
 void Ui::draw()
@@ -70,6 +89,7 @@ void Ui::draw()
 	StateHandler::getInstance().m_pWindow->setView(m_view);
 
 	StateHandler::getInstance().m_pWindow->draw(m_uiBackgroundSprite);
+	StateHandler::getInstance().m_pWindow->draw(m_healthDiamondSprite);
 	StateHandler::getInstance().m_pWindow->draw(rightSideText);
 	StateHandler::getInstance().m_pWindow->draw(rightSideStats);
 	StateHandler::getInstance().m_pWindow->draw(rightSideSecondaryText);
