38171c40c9f6494d219d1fdb3e25a466bfd555ae
Christian Holt <ymabob@gmail.com>
2012-11-09 15:57:22 +0100

    Fixed compiling in release mode

--- GameProject/GameProject/Enemy.cpp
+++ GameProject/GameProject/Enemy.cpp
@@ -8,6 +8,18 @@ void Enemy::setCollisionMap(std::vector<bool>* collisionMap, int horizontalSize)
 	this -> collisionMap = collisionMap;
 }
 
+/// adds 10 for orthogonal movement, 14 for diagonal. Precalculating this avoids any 
+/// decimal and square root calculations during runtime. The pythagorean theroem states that 
+/// c^2 = b^2 + a^2, giving c = sqroot(1^2 + 1^2) = sqroot(2) as the answer.
+/// The square root of 2 is 1.414, hence 14. 
+#define ORTHOGONAL_MOVEMENT_COST 10;	// Orthogonal == horizontal || vertical. 
+#define DIAGONAL_MOVEMENT_COST 14;
+
+// C++ only defines null if any standard libraries are included. 
+#ifndef NULL
+#define NULL 0
+#endif
+
 	/*!
 	 Pathfinding algorithm (A*) using a Binary Heap representation of the openList. 
 
@@ -34,117 +46,276 @@ void Enemy::setCollisionMap(std::vector<bool>* collisionMap, int horizontalSize)
 	*/
 Vector2f* Enemy::findPath(int startX, int startY, int goalX, int goalY)
 {
-	std::list<Point*> closedList;
-	std::list<Point*> openList;
-	std::list<Point*>::iterator it;
+	const short OPEN_LIST_ELEMENTS = 500;
+	const short NUMBER_OF_TILES_TO_CHECK = 20;
 
-	Point* openHeapList[400];
+	short openList[OPEN_LIST_ELEMENTS];
 
-	bool doNotCheck = false;
+	short fCost[OPEN_LIST_ELEMENTS];
+	short openX[OPEN_LIST_ELEMENTS];
+	short openY[OPEN_LIST_ELEMENTS];
 
-	const short NUMBER_OF_TILES_TO_CHECK = 45;
-	short tilesChecked = 0;
-	
+	short squaresChecked = 0;			// total number of items added to the open list. 
+	short tilesChecked = 0;				// total number of items added to the closed list. 
+
+	short numberOfOpenListItems = 0;	// Number of items currently in the open list
+	short numberOfClosedListItems = 0;	// Number of items currently in the closed list
+
 	short currentRow = startY / TILESIZE;
 	short currentColumn = startX / TILESIZE;
 	short currentPos = 0;						// Current position in collision vector (1D)
-	short lowestFCost = 10000;
-	short tentativeGScore = 0;
 
-	Point* current = new Point(currentRow, currentColumn);
-	Point* goal = new Point(goalX / TILESIZE, goalY / TILESIZE);
-	Point* adjacent;
+	short m = 0;
+	short current = 0;
+	short u = 1;	// Temporary bullshit variables... should be renamed to convey what they do. 
+	short v = 1;
+	short temp = 0;
 
-	openList.push_back(current);
-	current -> inOpenList = true;
-	current -> findGCost();
-	current -> findFCost(goal);
-
-	while(!openList.empty() && tilesChecked < NUMBER_OF_TILES_TO_CHECK)
-	{
-		// Pick lowest FCost -- SLOOOOOW PART..
-		for(it = openList.begin(); it != openList.end(); ++it)
-		{
-			short fCost = (*it) -> getFCost();
-
-			if(fCost < lowestFCost)
-			{
-				lowestFCost = fCost;
-				current = (*it);
-			}
-		}
-		
-		currentRow = current -> yPosition;
-		currentColumn = current -> xPosition;
-		
-		openList.remove(current);
-		closedList.push_front(current);
-		current -> inOpenList = false;
-		current -> inClosedList = true;
-
-
-		if(current == goal)
-		{
-			std::cout << "Does it ever go here?" << std::endl;
-			return new Vector2f(current -> xPosition * TILESIZE, current -> yPosition * TILESIZE);
-		}
-
-
-		// Check all adjacent tiles. 
-		for(int y = -1; y <= 1; ++y)
-		{
-			for(int x = -1; x <= 1; ++x)
-			{
-				if( !(x == 0 && y == 0) )		// If not self.
-				{
-					doNotCheck = false;
-
-					// Check map boundaries. 
-					if((currentRow == 0 && y == -1) || ( currentRow == m_VerticalBitmapSize && y == 1 ))
-					{
-						doNotCheck = true;
-					}
-
-					if(( currentColumn == 0 && x == -1 ) || ( currentColumn == m_HorizontalBitmapSize && x == 1 ))
-					{
-						doNotCheck = true;
-					}
-
-
-					if(!doNotCheck)
-					{
-						currentPos = ((currentRow + y) * m_HorizontalBitmapSize) + (currentColumn + x);
-
-						//std::cout << currentPos << std::endl;
-						if( (*collisionMap)[currentPos] )		// If walkable. 
-						{
-							adjacent = new Point(currentColumn + x, currentRow + y);
-							adjacent -> setParent(current);
-
-							if(!adjacent -> inClosedList)	// not in closed List
-							{
-								tentativeGScore = adjacent -> findGCost();
-
-								if(!adjacent -> inOpenList || tentativeGScore <= adjacent -> getGCost())	// If not in openList
-								{
-									adjacent -> g_cost = tentativeGScore;
-									adjacent -> findFCost(goal);
-
-									if(!adjacent -> inOpenList)
-									{
-										openList.push_front(adjacent);
-										adjacent -> inOpenList = true;
-									}
-								}
-							}
-						}
-					}
-				}
-			}
-		}
-
-		tilesChecked++;
-	}
+	bool goalReached = false;
+	bool placeInHeapFound = false;
 
 	return new Vector2f(startX, startY);
+
+	while(!goalReached && tilesChecked < NUMBER_OF_TILES_TO_CHECK)
+	{
+		
+
+		// Deleting & Selecting
+
+			current = openList[1];
+			openList[1] = openList[numberOfOpenListItems];
+			numberOfOpenListItems--;
+
+			if(openX[current] == goalX / TILESIZE && openY[current] == goalY / TILESIZE)
+			{
+				goalReached = true;
+			}
+
+
+			placeInHeapFound = false;
+
+
+			v = 1;
+			
+			while(!placeInHeapFound)			// Find the place in the heap for the element we moved. 
+			{
+				u = v;
+
+				if(2 * u + 1 <= numberOfOpenListItems)	// if both children exists. 
+				{
+					if(fCost[openList[u]] >= fCost[openList[ 2 * u]])		// Check first child.
+					{
+						v = 2 * u;
+					}
+					
+					if(fCost[openList[v]] >= fCost[openList[2 * u + 1]])	// Check second child.
+					{
+						v = 2 * u + 1;
+					}
+				}
+				else if(2 * u <= numberOfOpenListItems)	// If only 1 child.
+				{
+					if(fCost[openList[u]] >= fCost[openList[ 2 * u]])		// Check first child.
+					{
+						v = 2 * u;
+					}
+				}
+
+
+				if(u != v)					// If 'v' was changed, a swap is needed.
+				{
+					temp = openList[u];
+					openList[u] = openList[v];
+					openList[v] = temp;
+				}
+				else
+				{
+					placeInHeapFound = true;
+				}
+			}
+			
+
+
+
+
+
+
+		// insertion.
+
+			squaresChecked++;
+			numberOfOpenListItems++;
+			openList[numberOfOpenListItems] = squaresChecked;
+
+			m = numberOfOpenListItems;
+
+			while(m > 1)
+			{
+				// Check if child is <= parent, if so, swap.
+				if(fCost[openList[ m ]] <= fCost[openList[ m / 2 ]])
+				{
+					temp = openList[ m / 2 ];
+					openList[ m / 2 ] = openList[ m ];
+					openList[m] = temp;
+				
+					m = m / 2;
+				}
+				else
+				{
+					m = 0;
+				}
+			}
+
+		// insertion end
+
+
+
+
+
+
+	}
+
 }
+
+
+
+
+
+
+
+
+
+
+
+	//std::list<Point*> closedList;
+	//std::list<Point*> openList;
+	//std::list<Point*>::iterator it;
+
+	//Point* openHeapList[400];
+
+	//bool doNotCheck = false;
+
+	//const short NUMBER_OF_TILES_TO_CHECK = 20;
+	//short tilesChecked = 0;
+	//
+	//short currentRow = startY / TILESIZE;
+	//short currentColumn = startX / TILESIZE;
+	//short currentPos = 0;						// Current position in collision vector (1D)
+	//short lowestFCost = 10000;
+	//short tentativeGScore = 0;
+
+	//Point* current = new Point(currentRow, currentColumn);
+	//Point* goal = new Point(goalX / TILESIZE, goalY / TILESIZE);
+	//Point* adjacent;
+
+	//openList.push_back(current);
+	//current -> inOpenList = true;
+	//current -> findGCost();
+	//current -> findFCost(goal);
+
+	//while(!openList.empty() && tilesChecked < NUMBER_OF_TILES_TO_CHECK)
+	//{
+	//	lowestFCost = 100000;
+	//	// Pick lowest FCost -- SLOOOOOW PART..
+	//	for(it = openList.begin(); it != openList.end(); ++it)
+	//	{
+	//		short fCost = (*it) -> getFCost();
+
+	//		if(fCost < lowestFCost)
+	//		{
+	//			lowestFCost = fCost;
+	//			current = (*it);
+	//		}
+	//	}
+	//	
+	//	currentRow = current -> yPosition;
+	//	currentColumn = current -> xPosition;
+
+	//	std::cout << "Before: ";
+	//	for(it = openList.begin(); it != openList.end(); ++it)
+	//	{
+	//		std::cout << (*it) -> xPosition << ", " << (*it) -> yPosition << ", " << (*it) -> inOpenList << ", " << (*it) -> inClosedList << " ... ";
+	//	}
+
+	//	openList.remove(current);
+
+	//	std::cout << "\nAfter:  ";
+
+	//	for(it = openList.begin(); it != openList.end(); ++it)
+	//	{
+	//		std::cout << (*it) -> xPosition << ", " << (*it) -> yPosition << ", " << (*it) -> inOpenList  << ", " << (*it) -> inClosedList << " ... ";
+	//	}
+
+	//	std::cout << "\n\n";
+
+	//	char a;
+	//	std::cin >> a;
+
+	//	closedList.push_front(current);
+	//	current -> inOpenList = false;
+	//	current -> inClosedList = true;
+
+
+	//	if(current == goal)
+	//	{
+	//		std::cout << "Does it ever go here?" << std::endl;
+	//		return new Vector2f(current -> xPosition * TILESIZE, current -> yPosition * TILESIZE);
+	//	}
+
+
+	//	// Check all adjacent tiles. 
+	//	for(int y = -1; y <= 1; ++y)
+	//	{
+	//		for(int x = -1; x <= 1; ++x)
+	//		{
+	//			if( !(x == 0 && y == 0) )		// If not self.
+	//			{
+	//				doNotCheck = false;
+
+	//				// Check map boundaries. 
+	//				if((currentRow == 0 && y == -1) || ( currentRow == m_VerticalBitmapSize && y == 1 ))
+	//				{
+	//					doNotCheck = true;
+	//				}
+
+	//				if(( currentColumn == 0 && x == -1 ) || ( currentColumn == m_HorizontalBitmapSize && x == 1 ))
+	//				{
+	//					doNotCheck = true;
+	//				}
+
+
+	//				if(!doNotCheck)
+	//				{
+	//					currentPos = ((currentRow + y) * m_HorizontalBitmapSize) + (currentColumn + x);
+
+	//					//std::cout << currentPos << std::endl;
+	//					if( (*collisionMap)[currentPos] )		// If walkable. 
+	//					{
+	//						adjacent = new Point(currentColumn + x, currentRow + y);
+	//						adjacent -> setParent(current);
+
+	//						if(!adjacent -> inClosedList)	// not in closed List
+	//						{
+	//							tentativeGScore = adjacent -> findGCost();
+
+	//							if(!adjacent -> inOpenList || tentativeGScore <= adjacent -> getGCost())	// If not in openList
+	//							{
+	//								adjacent -> g_cost = tentativeGScore;
+	//								adjacent -> findFCost(goal);
+
+	//								if(!adjacent -> inOpenList)
+	//								{
+	//									openList.push_front(adjacent);
+	//									adjacent -> inOpenList = true;
+	//								}
+	//							}
+	//						}
+	//					}
+	//				}
+	//			}
+	//		}
+	//	}
+
+	//	tilesChecked++;
+	//}
+
+	//return new Vector2f(10 * 96, 12 * 96);
--- GameProject/GameProject/Point.h
+++ GameProject/GameProject/Point.h
@@ -1,53 +1,43 @@
-#pragma once
-
-#include "Vector2f.h"
-#include "Math.h"		// - abs
-
-/// adds 10 for orthogonal movement, 14 for diagonal. Precalculating this avoids any 
-/// decimal and square root calculations during runtime. The pythagorean theroem states that 
-/// c^2 = b^2 + a^2, giving c = sqroot(1^2 + 1^2) = sqroot(2) as the answer.
-/// The square root of 2 is 1.414, hence 14. 
-#define ORTHOGONAL_MOVEMENT_COST 10;	// Orthogonal == horizontal || vertical. 
-#define DIAGONAL_MOVEMENT_COST 14;
-
-// C++ only defines null if any standard libraries are included. 
-#ifndef NULL
-#define NULL 0
-#endif
-
-/// This class represents a point(node) on the tilemap (bitmap) used in A* pathfinding.
-class Point
-{
-public:
-//	Vector2f m_Position;
-	short xPosition;
-	short yPosition;
-	Point* m_Parent;
-
-	short f_cost;
-	short g_cost;
-	short h_cost;
-
-	Point();
-	Point(int x, int y);
-	
-	void  setParent(Point* parent);
-	short findGCost();
-	short findHCost(Point* goal);
-	short findFCost(Point* goal);
-	Vector2f* findPosition();
-	
-	bool inOpenList;
-	bool inClosedList;
-
-	bool hasParent();
-
-	//bool operator== (Point* point);
-
-	// GETTERS
-	//Point* getParent();
-	short getFCost();
-	short getGCost();
-	short getHCost();
-
-};
+//#pragma once
+//
+//#include "Vector2f.h"
+//#include "Math.h"		// - abs
+//
+//
+//
+///// This class represents a point(node) on the tilemap (bitmap) used in A* pathfinding.
+//class Point
+//{
+//public:
+////	Vector2f m_Position;
+//	short xPosition;
+//	short yPosition;
+//	Point* m_Parent;
+//
+//	short f_cost;
+//	short g_cost;
+//	short h_cost;
+//
+//	Point();
+//	Point(int x, int y);
+//	
+//	void  setParent(Point* parent);
+//	short findGCost();
+//	short findHCost(Point* goal);
+//	short findFCost(Point* goal);
+//	Vector2f* findPosition();
+//	
+//	bool inOpenList;
+//	bool inClosedList;
+//
+//	bool hasParent();
+//
+//	bool operator== (Point* point);
+//
+//	// GETTERS
+//	//Point* getParent();
+//	short getFCost();
+//	short getGCost();
+//	short getHCost();
+//
+//};
